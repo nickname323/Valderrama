b9dea18301f072bd8224f60562a8cd92aa5ebcce
NelsonRaf <nicknameispro323@gmail.com>
2026-03-14 16:49:18 +0800

    Add files via upload
    new

---
+++ StudReg/ComputeAverageApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ComputeAverageApp
+{
+    class ComputeAverageProgram
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            double[] grades = new double[5];
+
+            Console.WriteLine("Enter 5 grades separated by new line:");
+
+            for (int i = 0; i < 5; i++)
+            {
+                grades[i] = Convert.ToDouble(Console.ReadLine());
+                sum += grades[i];
+            }
+
+            double average = sum / 5;
+            double rounded = Math.Round(average);
+
+            Console.WriteLine("The average is " + average + " and round off to " + rounded);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StudReg/frmStudentGradeProgram.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace StudentGradeApplicationn
+{
+    public partial class frmStudentGradeProgram : Form
+    {
+        public frmStudentGradeProgram()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                if (txtName.Text == "" ||
+                    txtEnglish.Text == "" ||
+                    txtMath.Text == "" ||
+                    txtScience.Text == "" ||
+                    txtFilipino.Text == "" ||
+                    txtHistory.Text == "")
+                {
+                    MessageBox.Show("Please complete all fields.");
+                    return;
+                }
+
+                
+                double eng = Convert.ToDouble(txtEnglish.Text);
+                double math = Convert.ToDouble(txtMath.Text);
+                double sci = Convert.ToDouble(txtScience.Text);
+                double fil = Convert.ToDouble(txtFilipino.Text);
+                double hist = Convert.ToDouble(txtHistory.Text);
+
+                
+                double average = (eng + math + sci + fil + hist) / 5;
+
+               
+                string remarks = average >= 75 ? "The student passed." : "The student failed.";
+
+                
+                lblResult.Text = remarks +
+                    "\nThe general average of " + txtName.Text +
+                    " is " + average.ToString("F2") + ".";
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Numbers only for grades!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
