17290cb94a504aa3d216f60414652e5ef46d1768
Nelson <nicknameispro323@gmail.com>
2026-03-22 20:19:38 +0800

    tp

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WordGuessApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblWord = new System.Windows.Forms.Label();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.lstWrong = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblAttempts = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblWord
+            // 
+            this.lblWord.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.lblWord.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWord.ForeColor = System.Drawing.Color.White;
+            this.lblWord.Location = new System.Drawing.Point(67, 9);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(348, 76);
+            this.lblWord.TabIndex = 0;
+            this.lblWord.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGuess.Location = new System.Drawing.Point(129, 88);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(211, 33);
+            this.txtGuess.TabIndex = 1;
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnGuess.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuess.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuess.ForeColor = System.Drawing.Color.White;
+            this.btnGuess.Location = new System.Drawing.Point(129, 148);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(211, 50);
+            this.btnGuess.TabIndex = 2;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = false;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // lstWrong
+            // 
+            this.lstWrong.FormattingEnabled = true;
+            this.lstWrong.Location = new System.Drawing.Point(421, 37);
+            this.lstWrong.Name = "lstWrong";
+            this.lstWrong.Size = new System.Drawing.Size(135, 108);
+            this.lstWrong.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(421, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Wrong guesses";
+            // 
+            // lblAttempts
+            // 
+            this.lblAttempts.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.lblAttempts.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAttempts.Location = new System.Drawing.Point(177, 124);
+            this.lblAttempts.Name = "lblAttempts";
+            this.lblAttempts.Size = new System.Drawing.Size(121, 23);
+            this.lblAttempts.TabIndex = 5;
+            this.lblAttempts.Text = "Attempts Left: 5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 261);
+            this.Controls.Add(this.lblAttempts);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstWrong);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.lblWord);
+            this.Name = "Form1";
+            this.Text = "Guess the Word";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWord;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.ListBox lstWrong;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblAttempts;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WordGuessApplication
+{
+    public partial class Form1 : Form
+    {
+        
+        List<string> words = new List<string>()
+        {
+            "computer", "science", "program", "network", "monitor"
+        };
+
+        string correctWord;
+        StringBuilder displayWord = new StringBuilder();
+        int attemptsLeft = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            Random rnd = new Random();
+            correctWord = words[rnd.Next(words.Count)];
+
+            displayWord.Clear();
+            lstWrong.Items.Clear();
+            attemptsLeft = 5;
+
+            for (int i = 0; i < correctWord.Length; i++)
+            {
+                if (i == 0 || i == correctWord.Length - 1)
+                    displayWord.Append(correctWord[i]);
+                else
+                    displayWord.Append('?');
+            }
+
+            lblWord.Text = displayWord.ToString();
+            lblAttempts.Text = "Attempts Left: " + attemptsLeft;
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            string guess = txtGuess.Text.ToLower();
+
+            if (string.IsNullOrWhiteSpace(guess))
+                return;
+
+            
+            if (lstWrong.Items.Contains(guess))
+            {
+                MessageBox.Show("Already guessed!");
+                return;
+            }
+
+            if (guess == correctWord)
+            {
+                lblWord.Text = correctWord;
+                MessageBox.Show("Correct guess!");
+                StartGame();
+            }
+            else
+            {
+                lstWrong.Items.Add(guess);
+                attemptsLeft--;
+
+                lblAttempts.Text = "Attempts Left: " + attemptsLeft;
+
+                if (attemptsLeft == 0)
+                {
+                    MessageBox.Show("Game Over! The word was: " + correctWord);
+                    StartGame();
+                }
+                else
+                {
+                    MessageBox.Show("Wrong guess! Try again.");
+                }
+            }
+
+            txtGuess.Clear();
+        }
+    }
+}
